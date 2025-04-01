9d357e5d25386b8616a1a18f6c26976a6d1a7d3a
rohan1891 <rohangope588@gmail.com>
2025-04-01 21:03:10 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function login() {
+    alert("Login feature coming soon!");
+}
+
+function register() {
+    alert("Registration feature coming soon!");
+}
+
+function placeTrade(color) {
+    alert("Trade placed on " + color);
+}
